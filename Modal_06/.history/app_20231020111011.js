f194748cc83ae8e181072f064a1d793743f4c05e
sunnymsegs <sunnymsegs@gmail.com>
2023-10-20 14:29:56 +0530

    added Modal

---
+++ Modal_06/.history/app_20231020111011.js
@@ -0,0 +1,17 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn=document.querySelector('.modal-btn');
+const modalOverLay=document.querySelector('.modal-overlay');
+const closeBtn=document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click',function(){
+    modalOverLay.classList.add('open-modal')
+})
+
+closeBtn.addEventListener('click',function(){
+    if(modalOverLay.classList.co)
+    modalOverLay.classList.remove('.open-modal')
+})
